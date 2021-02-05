7e0303dd92f684b36e9833047389bf825d0c27b5
germanespinosa <germanespinosa@gmail.com>
2021-02-05 13:38:17 -0600

    none

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,15 @@
 import subprocess
+import glob
 
 processes = {}
-processes["habitat"] = subprocess.Popen(['python3', 'tests/test_habitat.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-processes["api"] = subprocess.Popen(['python3', 'tests/test_api.py'], stdout=subprocess.PIPE,  stderr=subprocess.STDOUT)
-processes["remote"] = subprocess.Popen(['python3', 'tests/test_remote.py'], stdout=subprocess.PIPE,  stderr=subprocess.STDOUT)
-processes["console"] = subprocess.Popen(['python3', 'tests/test_console.py'], stdout=subprocess.PIPE,  stderr=subprocess.STDOUT)
+
+for test_file in glob.glob("tests/*"):
+    test_name = test_file.split("/")[1].replace("test_","").replace(".py","")
+    processes[test_name] = subprocess.Popen(['python3', test_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 print("tests running..")
 for test in processes.keys():
-    print(test + " tests : ", end="")
+    print(test_name + " tests : ", end="")
     if processes[test].wait() != 0:
         print("fail")
         for output in processes[test].stdout.readlines():
